49feaf61f8367e2afee763c6c3cd20d292a7f6f2
dafiest-hub <angelcovarrubias319@gmail.com>
2025-01-20 12:22:27 -0600

    Update main.py
    version 7

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ led = Pin(2, Pin.OUT)
 def main():
     while True:
         led.value(not led.value())  # Cambia el estado del LED
-        sleep(0.3)  # Espera 1 segundo
+        sleep(0.1)  # Espera 1 segundo
 
 
 if __name__ == "__main__":
